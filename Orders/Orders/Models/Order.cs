855d67871f108577323d60ca1e3e2bc5d42cb33d
shivaspk <shivaspk.siva@gmail.com>
2021-01-27 05:04:14 +0530

    .netcode updated

---
+++ Orders/Orders/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Orders.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+        public int Id { get; set; }
+        public string buyer_address { get; set; }
+        public string buyer_name { get; set; }
+        public string buyer_email { get; set; }
+        public string buyer_phone { get; set; }
+        public string create_time { get; set; }
+        public string order_amount { get; set; }
+        public string order_status { get; set; }
+        public string update_time { get; set; }
+
+    }
+}
---
+++ Orders/Orders/Models/OrdersContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Orders.Models
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
